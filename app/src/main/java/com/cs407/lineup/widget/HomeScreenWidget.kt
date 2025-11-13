efbdbd113a59cecde46999617e3efd63ab475821
Ella Santoro <isantoro@wisc.edu>
2025-11-12 23:45:24 -0600

    basic widget skeleton from android glance

---
+++ app/src/main/java/com/cs407/lineup/widget/HomeScreenWidget.kt
@@ -0,0 +1,10 @@
+package com.cs407.lineup.widget
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class MyReceiver : GlanceAppWidgetReceiver() {
+
+    // Let MyAppWidgetReceiver know which GlanceAppWidget to use
+    override val glanceAppWidget: GlanceAppWidget = MyAppWidget()
+}
